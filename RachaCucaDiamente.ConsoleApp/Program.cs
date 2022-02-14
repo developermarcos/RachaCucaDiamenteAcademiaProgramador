614085ff459aaaa07693886a826dc1c417216ec3
Marcos Lima <developermarcos@outlook.com>
2022-02-14 18:04:17 -0300

    Exercício incompleto

--- RachaCucaDiamente.ConsoleApp/Program.cs
+++ RachaCucaDiamente.ConsoleApp/Program.cs
@@ -6,7 +6,29 @@ namespace RachaCucaDiamente.ConsoleApp
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string imprimir = "";
+            int alturaDiamante, quantidadeEspacoParaCaraLado, x = 1;
+            
+            Console.WriteLine("Escreva um valor: ");
+            alturaDiamante = Convert.ToInt32(Console.ReadLine());
+            quantidadeEspacoParaCaraLado = (alturaDiamante -1) / 2;
+            
+            for (int i = quantidadeEspacoParaCaraLado; i > 0; i--)
+            {
+                for (int z = 1; z <= quantidadeEspacoParaCaraLado; z++)
+                {
+                    imprimir += " ";
+                }
+                for (int j = 1; j <= x; j++)
+                {
+                    imprimir += "X";
+                }
+                
+                x += 2;
+                imprimir += "\n";
+            }
+            Console.WriteLine("Resultado final\n"+imprimir);
+            Console.ReadLine();
         }
     }
 }
