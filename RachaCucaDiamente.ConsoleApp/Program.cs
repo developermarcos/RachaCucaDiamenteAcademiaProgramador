8c03a1293add250a3bb1ff6bca7f381b2342739e
Marcos Lima <developermarcos@outlook.com>
2022-02-15 16:12:37 -0300

    Comentários processamento

--- RachaCucaDiamente.ConsoleApp/Program.cs
+++ RachaCucaDiamente.ConsoleApp/Program.cs
@@ -14,27 +14,26 @@ namespace RachaCucaDiamente.ConsoleApp
             quantidadeEspacoParaCaraLado = (alturaDiamante -1) / 2;
             //Processamento
             for (int i = alturaDiamante; i > 0; i--)
-            {
-                if (i > (quantidadeEspacoParaCaraLado +1))
+            {   //Parte de cima diamante
+                if (i >= quantidadeEspacoParaCaraLado)
                 {
                     for (int j = i; j > quantidadeEspacoParaCaraLado; j--)
                         imprimir += " ";
-                    
+
                     for (int h = 0; h < quantidadeX; h++)
                         imprimir += "X";
-                    
 
                     quantidadeX += 2;
                     imprimir += "\n";
                 }
-                else
+                else //Parte debaixo diamante
                 {
                     for (int z = (quantidadeEspacoParaCaraLado + 1); z >= i; z--)
                         imprimir += " ";
-                    
+
                     for (int h = 0; h < quantidadeX; h++)
                         imprimir += "X";
-                    
+
                     imprimir += "\n";
                     quantidadeX -= 2;
                 }
